23d40f497c515258cb9176b23c602599a946c697
unknown <aufamutawakkil@github.com>
2016-03-05 10:52:52 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="bower_components\bootstrap\dist\css/bootstrap.min.css"/>
+	<script type="text/javascript" src="bower_components\bootstrap\dist\css/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+	<!--Start of Zopim Live Chat Script-->
+<script type="text/javascript">
+window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+$.src="//v2.zopim.com/?3jzQk1hmTcC1XAxqZRFFMVI8boz0siuu";z.t=+new Date;$.
+type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+</script>
+<!--End of Zopim Live Chat Script-->
+
+</head>
+<body>
+	<div class="container-fluid">
+		
+		<div id='menu' class="row">
+			<div class="col-md-12 text-center">
+				<!-- logo -->
+				<div class='logo-container'>
+					<img src="img/logo.png" width="70px" />
+				</div>
+				<!-- navigation bar -->
+				<ul class='nav nav-pills menu'>
+					<li role="presentation"><a href="#support">homeNe</a></li>
+					<li role="presentation"><a href="#portofolio">Portofolio</a></li>
+					<li role="presentation"><a href="#pemesanan">Pemesanan</a></li>
+					
+					<li role="presentation"><a href="#galeri">Galeri</a></li>
+					<li role="presentation"><a href="#galeri">Cara Pemesanan</a></li>
+					<li role="presentation"><a href="#kontak">Kontak</a></li>
+				</ul>
+			</div>
+		</div>
+		
+		<div id='home' class="row">
+			<div class="col-md-12 home text-center">
+				<p> J A S A  &nbsp; B O R D I R &nbsp;<br/> P R O F E S S I O N A L </p>
+				<br/>
+				<div class="btn btn-xl"> PESAN SEKARANG ! </div>
+			</div>		
+		</div>
+		
+		<div id='support' class="row">
+			<div class="col-md-12 support text-center">
+				<p class='title'> PORTOFOLIO </p>
+				<ul>
+					<li>
+						<img src="img/topi.jpg" width="300px">
+					</li>
+					<li>
+						<img src="img/baju.jpg" width="300px">
+					</li>
+					<li>
+						<img src="img/logo.jpg" width="300px">
+					</li>
+				</ul>
+			</div>		
+		</div>
+
+
+		<div id='news' class="row"><div class="col-md-12 news text-center">
+				<p class='title'> BERITA </p>
+				<ul>
+					<li>
+						<img src="img/promo.jpg" width="225px">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+						<div class="btn-news pull-right"> Detail > </div>
+					</li>
+					<li>
+						<img src="img/promo.jpg" width="225px">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+						<div class="btn-news pull-right"> Detail > </div>
+					</li>
+					<li>
+						<img src="img/promo.jpg" width="225px">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+						<div class="btn-news pull-right"> Detail > </div>
+					</li>
+					<li>
+						<img src="img/promo.jpg" width="225px">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+						<div class="btn-news pull-right"> Detail > </div>
+					</li>
+				</ul>
+			</div>		
+		</div>
+
+		<div id='testimonial' class="row">
+			<div class="col-md-12 text-center">
+				<p class='title'> Testimonial </p>
+				<br/>
+				<ul>
+					<li class='text-center'>
+						<div class="icon-testimonial" style='background:url(img/icon.jpg)'></div>
+						<br/>
+						<div class="text-testimonial">
+							<span class='pull-left'>Saya Sangat Beruntung sekali</span>
+							<br/>
+							<span class='pull-right from'>Andre - Ceo Maximu</span>
+						</div>
+					</li>
+
+					<li class='text-center'>
+						<div class="icon-testimonial" style='background:url(img/icon.jpg)'></div>
+						<br/>
+						<div class="text-testimonial">
+							<span class='pull-left'>Saya Sangat Beruntung sekali</span>
+							<br/>
+							<span class='pull-right from'>Andre - Ceo Maximu</span>
+						</div>
+					</li>
+
+					<li class='text-center'>
+						<div class="icon-testimonial" style='background:url(img/icon.jpg)'></div>
+						<br/>
+						<div class="text-testimonial">
+							<span class='pull-left'>Saya Sangat Beruntung sekali</span>
+							<br/>
+							<span class='pull-right from'>Andre - Ceo Maximu</span>
+						</div>
+					</li>
+
+				</ul>
+			</div>		
+		</div>
+
+
+		<div id='client' class="row">
+			<div class="col-md-12 text-center">
+			</div>		
+		</div>
+
+		<div id='about' class="row">
+			<div class="col-md-12 text-center">
+			</div>		
+		</div>
+
+		<div id='contact' class="row">
+			<div class="col-md-12 text-center">
+				
+			</div>		
+		</div>
+
+	</div>
+</body>
+</html>
